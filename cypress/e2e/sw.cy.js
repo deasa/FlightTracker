0c4675d6cd15f3cfe2ef297b3fccf74556f78976
Brendan Ashton <bgeorgeashton@gmail.com>
2022-09-20 08:27:07 -0600

    scraping wanna get away fares and printing to console

--- cypress/e2e/sw.cy.js
+++ cypress/e2e/sw.cy.js
@@ -7,10 +7,18 @@ describe('empty spec', () => {
     cy.get('#LandingAirBookingSearchForm_destinationAirportCode').type('SLC').then(() => {
       cy.get('#LandingAirBookingSearchForm_destinationAirportCode--item-0 > .actionable').click()
     })
-    cy.get('#LandingAirBookingSearchForm_departureDate').type("12/21")
+    cy.get('#LandingAirBookingSearchForm_departureDate').type("12/20")
     cy.get('#LandingAirBookingSearchForm_returnDate').type("12/31")
     cy.get('#LandingAirBookingSearchForm_submit-button').click()
     cy.wait(2000)
     cy.get('#form-mixin--submit-button').click()
+    cy.contains('eparting flights').parent().parent().find("[data-test='fare-button--wanna-get-away']").each(($elm) => {
+      var amount = $elm.text()
+      cy.log("outbound flight points = " + amount)
+    })
+    cy.contains('eturning flights').parent().parent().find("[data-test='fare-button--wanna-get-away']").each(($elm) => {
+      var amount = $elm.text()
+      cy.log("inbound flight points = " + amount)
+    })
   })
 })
